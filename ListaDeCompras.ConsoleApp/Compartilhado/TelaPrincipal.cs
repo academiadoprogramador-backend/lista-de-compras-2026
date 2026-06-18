9d5a98e3e0e6a42156e828bcce4ec88e16bf9572
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-06-18 19:46:12 -0300

    Adiciona infraestrutura de dados de Categoria

--- ListaDeCompras.ConsoleApp/Compartilhado/TelaPrincipal.cs
+++ ListaDeCompras.ConsoleApp/Compartilhado/TelaPrincipal.cs
@@ -1,7 +1,19 @@
+using ListaDeCompras.ConsoleApp.Modulos.ModuloCategoria;
+
 namespace ListaDeCompras.ConsoleApp.Compartilhado;
 
 public class TelaPrincipal
 {
+    private readonly RepositorioCategoria repositorioCategoria;
+
+    public TelaPrincipal()
+    {
+        Categoria categoriaTeste = new Categoria("Produtos de Limpeza", CorCategoria.Vermelho);
+
+        repositorioCategoria = new RepositorioCategoria();
+        repositorioCategoria.Cadastrar(categoriaTeste);
+    }
+
     public ITelaOpcoes? ObterOpcaoMenuPrincipal()
     {
         Console.WriteLine("---------------------------------");
