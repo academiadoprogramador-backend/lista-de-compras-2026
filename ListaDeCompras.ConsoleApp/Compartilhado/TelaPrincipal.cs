af23d79f7dcc772963d67f5c555644117024e6e3
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-06-25 21:33:18 -0300

    Adiciona Itens de Lista de Compras

--- ListaDeCompras.ConsoleApp/Compartilhado/TelaPrincipal.cs
+++ ListaDeCompras.ConsoleApp/Compartilhado/TelaPrincipal.cs
@@ -1,4 +1,5 @@
 using ListaDeCompras.ConsoleApp.Modulos.ModuloCategoria;
+using ListaDeCompras.ConsoleApp.Modulos.ModuloItemListaCompras;
 using ListaDeCompras.ConsoleApp.Modulos.ModuloListaCompras;
 using ListaDeCompras.ConsoleApp.Modulos.ModuloProduto;
 
@@ -28,6 +29,7 @@ public class TelaPrincipal
         repositorioProduto.Cadastrar(produtoTeste);
 
         ListaCompras listaTeste = new ListaCompras("Compras do Mês");
+        listaTeste.AdicionarItem(new ItemListaCompras(produtoTeste, 3));
 
         repositorioListaCompras = new RepositorioListaCompras();
         repositorioListaCompras.Cadastrar(listaTeste);
@@ -54,7 +56,7 @@ public class TelaPrincipal
             return new TelaProduto(repositorioProduto, repositorioCategoria);
 
         if (opcaoMenuPrincipal == "3")
-            return new TelaListaCompras(repositorioListaCompras);
+            return new TelaListaCompras(repositorioListaCompras, repositorioProduto);
 
         return null;
     }
--- ListaDeCompras.ConsoleApp/Modulos/ModuloListaCompras/ListaCompras.cs
+++ ListaDeCompras.ConsoleApp/Modulos/ModuloListaCompras/ListaCompras.cs
@@ -2,7 +2,6 @@ using ListaDeCompras.ConsoleApp.Compartilhado;
 
 namespace ListaDeCompras.ConsoleApp.Modulos.ModuloListaCompras;
 
-
 public static class GeradorIdsListaCompras
 {
     private static int contadorIds = 1;
@@ -24,12 +23,40 @@ public class ListaCompras : EntidadeBase
     public string Nome { get; private set; }
     public DateTime DataCriacao { get; private set; }
     public StatusListaCompras Status { get; private set; } = StatusListaCompras.Aberta;
+    public ItemListaCompras[] Itens { get; private set; } = new ItemListaCompras[100];
 
     public ListaCompras(string nome)
     {
         Id = GeradorIdsListaCompras.GerarId();
         Nome = nome;
         DataCriacao = DateTime.Now;
+    }
+
+    public void AdicionarItem(ItemListaCompras itemLista)
+    {
+        for (int i = 0; i < Itens.Length; i++)
+        {
+            if (Itens[i] == null)
+            {
+                Itens[i] = itemLista;
+                return;
+            }
+        }
+    }
+
+    public void RemoverItem(int idItemLista)
+    {
+        for (int i = 0; i < Itens.Length; i++)
+        {
+            if (Itens[i] == null)
+                continue;
+
+            if (Itens[i].Id == idItemLista)
+            {
+                Itens[i] = null;
+                return;
+            }
+        }
     }
 
     public override void Atualizar(EntidadeBase entidadeAtualizada)
