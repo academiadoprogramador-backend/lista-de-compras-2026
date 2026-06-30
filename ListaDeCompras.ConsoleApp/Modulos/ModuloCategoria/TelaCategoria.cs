6173f3fb35b43d8c7b2b35585b6a747c2dd7af1a
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-06-30 20:21:31 -0300

    Adiciona Generics para classes Base

--- ListaDeCompras.ConsoleApp/Modulos/ModuloCategoria/TelaCategoria.cs
+++ ListaDeCompras.ConsoleApp/Modulos/ModuloCategoria/TelaCategoria.cs
@@ -3,7 +3,7 @@ using ListaDeCompras.ConsoleApp.Modulos.ModuloProduto;
 
 namespace ListaDeCompras.ConsoleApp.Modulos.ModuloCategoria;
 
-public class TelaCategoria : TelaBase, ITelaOpcoes
+public class TelaCategoria : TelaBase<Categoria>, ITelaOpcoes, ITelaCrud
 {
     private readonly RepositorioCategoria repositorioCategoria;
     private readonly RepositorioProduto repositorioProduto;
@@ -30,11 +30,11 @@ public class TelaCategoria : TelaBase, ITelaOpcoes
             "Id", "Nome", "Cor"
         );
 
-        EntidadeBase[] registros = repositorioCategoria.SelecionarTodos();
+        Categoria[] registros = repositorioCategoria.SelecionarTodos();
 
         for (int i = 0; i < registros.Length; i++)
         {
-            Categoria c = (Categoria)registros[i];
+            Categoria c = registros[i];
 
             if (c == null)
                 continue;
@@ -53,7 +53,7 @@ public class TelaCategoria : TelaBase, ITelaOpcoes
         }
     }
 
-    protected override EntidadeBase ObterDadosCadastrais()
+    protected override Categoria ObterDadosCadastrais()
     {
         Console.Write("Informe o nome da categoria: ");
         string? nome = Console.ReadLine();
@@ -97,20 +97,18 @@ public class TelaCategoria : TelaBase, ITelaOpcoes
         return new Categoria(nome!, cor);
     }
 
-    protected override bool ExisteRegistroComInformacoesExclusivas(EntidadeBase entidade, int? idIgnorado = null)
+    protected override bool ExisteRegistroComInformacoesExclusivas(Categoria entidade, int? idIgnorado = null)
     {
-        Categoria novaCategoria = (Categoria)entidade;
-
-        EntidadeBase[] categorias = repositorioCategoria.SelecionarTodos();
+        Categoria[] categorias = repositorioCategoria.SelecionarTodos();
 
         for (int i = 0; i < categorias.Length; i++)
         {
-            Categoria c = (Categoria)categorias[i];
+            Categoria c = categorias[i];
 
             if (c == null)
                 continue;
 
-            if (idIgnorado != c.Id && novaCategoria.Nome == c.Nome)
+            if (idIgnorado != c.Id && entidade.Nome == c.Nome)
             {
                 Console.WriteLine("---------------------------------");
                 Console.WriteLine($"Já existe uma categoria com o nome \"{c.Nome}\"!");
@@ -125,12 +123,11 @@ public class TelaCategoria : TelaBase, ITelaOpcoes
 
     protected override bool ExistemDependenciasAtivasDoRegistro(int idRegistro)
     {
-        // TODO - Não permitir excluir uma categoria caso tenha produtos vinculados
-        EntidadeBase[] produtos = repositorioProduto.SelecionarTodos();
+        Produto[] produtos = repositorioProduto.SelecionarTodos();
 
         for (int i = 0; i < produtos.Length; i++)
         {
-            Produto p = (Produto)produtos[i];
+            Produto p = produtos[i];
 
             if (p == null)
                 continue;
